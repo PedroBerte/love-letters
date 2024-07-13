cf1b7d4b3eb1efdafeebdc5dc5eebef998428801
PedroBerte <pedrobertelli19@gmail.com>
2024-07-13 13:36:31 -0300

    Criação da BottomTab e constants (colors).

---
+++ src/constants/colors.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  background: "#F4EDE9",
+  primaryBrown: "#8C6349",
+  primaryPink: "#F38E8C",
+  textDark: "#303030",
+  primaryGray: "#9FA3B3",
+  secondaryGray: "#F4F4F4",
+};
